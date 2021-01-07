956be72d2a396bd6cae2fbb8772718d0846bec26
erika618 <68714247+erika618@users.noreply.github.com>
2021-01-07 22:30:47 +0900

    messagesコントローラーの設定。

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,11 @@
 class MessagesController < ApplicationController
   def new
+    # 保存されているメッセージを全て表示したいため
+    @messages = Message.all
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(text: params[:message][:text])
   end
 end
